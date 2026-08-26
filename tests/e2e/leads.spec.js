6355751e610655da8d371b10edcc11dc9c48ce56
Marcos Oliveira <marcos.anderson.ao@gmail.com>
2026-08-26 15:33:36 -0300

    62. Substituindo Toast por Popup + Testes em paralelo

--- tests/e2e/leads.spec.js
+++ tests/e2e/leads.spec.js
@@ -8,8 +8,9 @@ test('deve cadastrar um lead na fila de cadastro', async ({ page }) => {
   await page.leads.visit();
   await page.leads.openLeadModal();
   await page.leads.submitLeadForm(leadName, leadEmail);
-
-  await page.toast.containText('Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!');
+  
+  const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato.';
+  await page.popup.haveText(message);
 });
 
 test('não deve cadastrar quando o email já existe', async ({ page, request }) => {
@@ -29,7 +30,8 @@ test('não deve cadastrar quando o email já existe', async ({ page, request }) 
   await page.leads.openLeadModal();
   await page.leads.submitLeadForm(leadName, leadEmail);
 
-  await page.toast.containText('O endereço de e-mail fornecido já está registrado em nossa fila de espera.');
+  const message = 'Verificamos que o endereço de e-mail fornecido já consta em nossa lista de espera. Isso significa que você está um passo mais perto de aproveitar nossos serviços.';
+  await page.popup.haveText(message);
 });
 
 test('não deve cadastrar com email incorreto', async ({ page }) => {
--- tests/support/actions/Components.js
+++ tests/support/actions/Components.js
@@ -1,19 +1,17 @@
 const { expect } = require('@playwright/test');
 
-export class Toast {
+export class Popup {
 
     constructor(page){
         this.page = page;
     }
 
-    async containText(message){
+    async haveText(message){
         /* await this.page.getByText('seus dados conosco').click;
         const content = await this.page.content();
         console.log(content); */                    //para propósito de debug, para capturar os dados do html
-        const toast = this.page.locator('.toast')
+        const popup = this.page.locator('.swal2-html-container')
 
-        await expect(toast).toContainText(message);
-        await expect(toast).not.toBeVisible({ timeout: 5000 });
-        //await this.page.waitForTimeout(5000);
+        await expect(popup).toContainText(message);
     }
 }
